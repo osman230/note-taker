3fbdb0396266468b2bd93f0ef589c6527de08615
Aisha Osman <aisha.osman09@gmail.com>
2021-11-14 15:17:49 -0600

    update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = proces.env.PORT || 3000;
 const database = require('./Develop/db/db.json');
+const { util } = require('prettier');
 
 
 //
@@ -12,8 +13,45 @@ app.use(express.static("public"));
 
 //routes
 
-require('./routes/apiRoutes/apiRoutes');
-require('./routes/htmlRoutes/htmlRoutes');
+                //HTML
+
+app.get('/notes', (request, respond) => {
+    respond.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+});
+app.get('*', (request,respond) => {
+    respond.sendFile(pathFile(__dirname, "./Develop/public/index.html"));
+});
+app.get('/index', (request, respond) => {
+    respond.sendFile(pathFile(__dirname, './Develop/public/index.html'));
+});
+
+
+                //API
+
+app.get("/api/notes", function(request, respond) {
+});
+
+const notes = JSON.parse(fs.readFileSync(database), 'utf8');
+const noteInfo = (notes.length).toString();
+
+app.post("/api/notes", function(request, respond) {
+    let addTask = req.body;
+    addTask.id = noteInfo;
+    notes.push(addTask);
+    fs.writeFileSync((database), JSON.stringify(notes));
+    respond.json(notes);
+});
+
+app.delete("/api/notes/:id", function(request, respond) {
+    let deleteTask = (req.params.id).toString();
+    notes = notes.filter(selected => {
+        return selected.id != deleteTask; 
+    });
+    fs.writeFileSync((database), JSON.stringify(notes));
+    respond.json(notes);
+});
+
+
 
 //listening
 
