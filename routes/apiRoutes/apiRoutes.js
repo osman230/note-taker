1af82701f89a16d30cd72d265f0fb0bc7120f87a
Aisha Osman <aisha.osman09@gmail.com>
2021-11-14 14:45:31 -0600

    routes

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,36 +1,19 @@
-const router = require('express').Router();
-const path = require('path');
 const fs = require('fs');
-const db = require('../../db/db.json');
+const database = require('./develop/db/db.json');
+const path = require('path');
 
-router.get('/notes', (req, res) => {
-    res.json(db);
-});
-
-router.post('/notes', (req, res) => {
-    let notes = db;
-    let newNote = req.body;
-    newNote.id = notes.length;
-    notes.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, '../../db/db.json'),
-        JSON.stringify(notes, null, 2)
-    );
-    res.json();
-});
-
-router.delete('/notes/:id', (req, res) => {
-    let notes = db;
-    let index = req.params.id;
-    notes.splice(index, 1);
-    notes.forEach(element => {
-        element.id = notes.indexOf(element);
+module.exports = function(app) {
+    app.get("/api/notes", function(request, respond) {
+        respond.json(database);
     });
-    fs.writeFileSync(
-        path.join(__dirname, '../../db/db.json'),
-        JSON.stringify(notes, null, 2)
-    );
-    res.json();
-});
 
-module.exports = router;
+    app.post("/api/notes", function(request, respond) {
+        database.push(req.body);
+        respond.json(true);
+    });
+
+    app.delete("/api/notes", function(request, respond) {
+        database.length = 0;
+        respond.json({ ok: true });
+    })
+};
--- routes/htmlRoutes/htmlRoutes.js
+++ routes/htmlRoutes/htmlRoutes.js
@@ -1,10 +1,13 @@
 const path = require('path');
 
-module.exports = function (app) {
-    app.get('/notes', function(req, res) {
-        res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
+module.exports = (app) => {
+    app.get('/notes', (request, respond) => {
+        respond.sendFile(path.join(__dirname, "./develop/public/notes.html"));
     });
-    app.get('*', function(req,res) {
-        res.sendFile(pathFile(__dirname, "./develop/public/index.html"));
+    app.get('*', (request,respond) => {
+        respond.sendFile(pathFile(__dirname, "./develop/public/index.html"));
+    });
+    app.get('/index', (request, respond) => {
+        respond.sendFile(pathFile(__dirname, './develop/public/index.html'));
     });
 };
