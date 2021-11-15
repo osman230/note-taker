b474ba289ed0aeb3438c2977079fe2cc53109eb4
Aisha Osman <aisha.osman09@gmail.com>
2021-11-15 11:27:42 -0600

    Server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 //
 app.use(express.json());
-app.use(express.static('./develop/public'));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 //listening
