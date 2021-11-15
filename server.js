5b0be8132eb73327a5cc12b724aa2a9183924e57
Aisha Osman <aisha.osman09@gmail.com>
2021-11-15 09:20:55 -0600

    server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlendcoded({ extended: true }));
+
+//listening
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
   });
