b62f20b1f89354db8b2994575f67494dd61f684c
Aisha Osman <aisha.osman09@gmail.com>
2021-11-15 12:50:29 -0600

    routes

--- routes/apiRoutes/apiRoutes.js
+++ routes/apiRoutes/apiRoutes.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
-const router = require('express').Router();
-// const express = require('express');
+const express = require('express');
 const path = require('path');
 const database = require('../../db/db.json');
 
@@ -10,15 +9,14 @@ const database = require('../../db/db.json');
 // const newData = task.filter(({id}) => id !== data);
 //
 
-router.get('/notes', function(res, req) {
+app.get('/notes', function(res, req) {
     res.json(database);
 });
 
-router.post("/notes", function(req,res) {
+app.post("/notes", function(req,res) {
     database.push(req.body);
     fs.readFileSync(path.join(__dirname, '../../db/db/json'), JSON.stringify(database, null, 2));
     res.json();
     });
 
 
-module.exports = router;
