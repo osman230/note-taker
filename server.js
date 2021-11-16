17d198a684819425d3d2ca026a32a3be5e6c04c2
Aisha Osman <aisha.osman09@gmail.com>
2021-11-16 10:08:11 -0600

    server

--- server.js
+++ server.js
@@ -1,29 +1,17 @@
-// need express to interact with the front end
-const express = require("express");
-// need path for filename paths
-const path = require("path");
-// need fs to read and write to files
 const fs = require("fs");
-
-// creating an "express" server
+const path = require("path");
+const express = require("express");
+const PORT = process.env.PORT || 3000;
 const app = express();
-// Sets an Initial port for listeners
-const PORT = process.env.PORT || 9001;
-
-//  Initialize newTask
-
-let newTask = [];
-
-// Set up body parsing, static, and route middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // api routes
 
-app.get("/api/notes", function(err, res) {
+let newTask = [];
+
+app.get("/api/notes", (req, res) => {
   try {
     newTask = fs.readFileSync("./db/db.json", "utf8");
     newTask = JSON.parse(newTask);
@@ -32,14 +20,14 @@ app.get("/api/notes", function(err, res) {
   res.json(newTask);
 });
 
-app.post("/api/notes", function(req, res) {
+app.post("/api/notes", (req, res) => {
   try {
     newTask = fs.readFileSync("./db/db.json", "utf8");
     newTask = JSON.parse(newTask);
     req.body.id = newTask.length;
     newTask.push(req.body);
     newTask = JSON.stringify(newTask);
-    fs.writeFile("./db/db.json", newTask, "utf8", function(err) {
+    fs.writeFile("./db/db.json", newTask, "utf8", (err) => {
       if (err) throw err;
     });
     res.json(JSON.parse(newTask));
@@ -50,19 +38,19 @@ app.post("/api/notes", function(req, res) {
 
 // html routes
 
-app.get("/notes", function(req, res) {
+app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get("/api/notes", function(req, res) {
+app.get("/api/notes", (req, res) => {
   return res.sendFile(path.json(__dirname, "./db/db.json"));
 });
 
-app.get("*", function(req, res) {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 // listening
-app.listen(PORT, function() {
-  console.log("SERVER IS LISTENING: " + PORT);
+app.listen(PORT, () => {
+  console.log("App is listening: " + PORT);
 });
