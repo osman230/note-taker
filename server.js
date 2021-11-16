69198b8a807fe9b518781493c207e6f7891b69de
Aisha Osman <aisha.osman09@gmail.com>
2021-11-16 09:32:21 -0600

    server

--- server.js
+++ server.js
@@ -12,9 +12,11 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
+
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
+
 app.get("/api/notes", function(req, res) {
   return res.sendFile(path.json(__dirname, "./db/db.json"));
 });
@@ -22,21 +24,42 @@ app.get("/api/notes", function(req, res) {
 // api routes 
 
 let task = [];
-
 app.get("/api/notes", function(err, res) {
     task = fs.readFileSync("./db/db.json", "utf8");
-    task = JSON.parse(task);
+    console.log("hello!");
+
+    task = JSON.parse(task);  
     res.json(task);
 });
 
+// writes the new note to the json file
 app.post("/api/notes", function(req, res) {
+  try {
+    // reads the json file
     task = fs.readFileSync("./db/db.json", "utf8");
+    console.log(task);
+
+    // parse the data to get an array of objects
     task = JSON.parse(task);
+    // Set new notes id
     req.body.id = task.length;
-    task.push(req.body);
+    // add the new note to the array of note objects
+    task.push(req.body); // req.body - user input
+    // make it string(stringify)so you can write it to the file
     task = JSON.stringify(task);
-    fs.writeFile("./db/db.json", task, "utf8")
+    // writes the new note to file
+    fs.writeFile("./db/db.json", task, "utf8", function(err) {
+      // error handling
+      if (err) throw err;
+    });
+    // changeit back to an array of objects & send it back to the browser(client)
     res.json(JSON.parse(task));
+
+    // error Handling
+  } catch (err) {
+    throw err;
+    console.error(err);
+  }
 });
 
 
